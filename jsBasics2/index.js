34db02936c244a9cfdb0d70b8355d7b946e7f400
ankitbhoi <ankitghosal2002@gmail.com>
2024-02-17 15:21:16 +0530

    -

--- jsBasics2/index.js
+++ jsBasics2/index.js
@@ -39,3 +39,47 @@ let storeVariable3 = new createRectangle(99,88);
 storeVariable3.color= "yellow";
 //delete a property
 delete storeVariable3.color;
+
+//reference type example
+let a={
+    value:10,
+};
+let b=a;
+a.value++;
+console.log(a.value);          //11
+console.log(b.value);          //11
+
+//loops
+let square = {
+    length:2,
+    breadth:3
+};
+
+//for in loop
+for(let key in square){
+    //keys are reflected key variable
+    //values are reflected through box braces => square[key]
+    console.log(square[key]);
+}
+
+//for of loop=>only used on iterables - array,maps
+// for(let key of square){
+//     console.log(key);
+//     //will give error bcz iterable hai nahi bhidu
+// }
+
+//object cloning
+let src = {
+    a:10,
+    b:11
+};
+
+let src2 = {
+    c:12
+};
+let dest = Object.assign({},src,src2);
+src.a++;
+src.c++;
+console.log(dest);
+
+
